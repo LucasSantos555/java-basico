9eebb8620406136d68943ac45ebd1be0fe4ba055
Lucas Santos <lucasaraujooliveirasantos755@gmail.com>
2026-05-26 18:07:06 -0300

    26.05.2026

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       /* */
+        short num = 1;
+       int num2 = num;
+       short num3= (short)num2; //Erro, 4 bytes não cabem em 2 bytes
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+        final double PI = 3.14;
+        
     }
 }
